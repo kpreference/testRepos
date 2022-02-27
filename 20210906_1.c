e4a25791433fc20dff7d2fa390eaf7b8411a08bd
= <ksh091411@naver.com>
2022-02-27 20:25:30 +0900

    20220227

---
+++ 20210906_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    
+    int i;
+    scanf("%d",&i);
+    printf("hello");
+    return 0;
+}
---
+++ 20210906_2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int n;
+	scanf("%d", &n);
+	if (n >= 100) {
+		printf("large");
+	}
+	else {
+		printf("small");
+	}
+}
